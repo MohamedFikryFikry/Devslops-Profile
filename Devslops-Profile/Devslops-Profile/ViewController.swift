a78b52a49dfa5f9e23d1f28367a5c42185697019
MOHAMED <mohamed@MOHAMEDs-iMac.local>
2019-10-04 10:08:19 +0200

    design for iphone

---
+++ Devslops-Profile/Devslops-Profile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Devslops-Profile
+//
+//  Created by MOHAMED on 10/4/19.
+//  Copyright © 2019 MOHAMED. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
